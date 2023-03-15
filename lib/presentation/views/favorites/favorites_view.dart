f7ef59d5a26a1ddd994b00bc5074fcb48da4803e
Kleyer Sosa <kleyer.sosa@Kleyer-Sosa-MacBook-Pro.local>
2023-03-15 12:05:02 -0600

    Favorites feature is ready.

--- lib/presentation/views/favorites/favorites_view.dart
+++ lib/presentation/views/favorites/favorites_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:pets_app/model/pet.dart';
 import 'package:pets_app/presentation/views/favorites/widgets/favorites_mobile_view.dart';
 import 'package:pets_app/presentation/views/favorites/widgets/favorites_web_view.dart';
+import 'package:pets_app/provider/pet_provider.dart';
 import 'package:pets_app/widgets/adaptive_layout_widget.dart';
+import 'package:provider/provider.dart';
 
 class FavoritesView extends StatelessWidget {
   const FavoritesView({Key? key}) : super(key: key);
@@ -9,13 +12,20 @@ class FavoritesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Favorites'),
-      ),
-      body: const AdaptiveLayoutWidget(
-        mobileWidget: FavoritesMobileView(),
-        webWidget: FavoritesWebView(),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('My Favorites'),
+        ),
+        body: Selector<PetProvider, List<Pet>>(
+          selector: (context, petProvider) => petProvider.petList,
+          builder: (context, value, child) => value.isNotEmpty
+              ? const AdaptiveLayoutWidget(
+                  mobileWidget: FavoritesMobileView(),
+                  webWidget: FavoritesWebView(),
+                )
+              : const Center(
+                  child: Text('No favorites'),
+                ),
+          shouldRebuild: (previous, next) => true,
+        ));
   }
 }
